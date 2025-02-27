4a217b3841d7b93dfac1173272ac27e53b893a6a
MaiTram01 <tramh7879@gmail.com>
2025-02-27 09:30:09 +0700

    Done

--- routes/web.php
+++ routes/web.php
@@ -13,3 +13,17 @@ Route::post('/products/store', [ProductController::class, 'store'])->name('produ
 Route::get('/products/{id}/edit', [ProductController::class, 'edit'])->name('products.edit');
 Route::put('/products/{id}', [ProductController::class, 'update'])->name('products.update');
 Route::delete('/products/{id}', [ProductController::class, 'destroy'])->name('products.destroy');
+
+
+Route::get('index', [PageController::class, 'getIndex'])->name('trang-chu');
+
+Route::get('loai-san-pham', [PageController::class, 'getLoaiSp'])->name('loaisanpham');
+		
+Route::get('chi-tiet-san-pham', [PageController::class, 'getChitiet'])->name('chitietsanpham');
+
+Route::get('lien_he', [PageController::class, 'getLienhe'])->name('lienhe');
+
+Route::get('gioi_thieu', [PageController::class, 'getAbout'])->name('about');
+
+
+
